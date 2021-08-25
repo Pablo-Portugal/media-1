3c3dffdf687813f079a03783ab76379316bb00ad
Pablo <pabloportugaldasilva01@gmail.com>
2021-08-25 16:33:54 -0300

    Versão 1 - 25-08

---
+++ src/mediaDoAluno/CalculaMediaDoAluno.java
@@ -0,0 +1,23 @@
+package mediaDoAluno;
+
+public class CalculaMediaDoAluno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	double nota1,nota2,nota3,nota4,resultado;
+	String nomedoaluno;
+	nota1=8.9;
+	nota2=7.6;
+	nota3=9.7;
+	nota4=6.7;
+	nomedoaluno="Pablo";
+	resultado=(nota1+nota2+nota3+nota4)/4;
+			
+	System.out.println("O(a) aluno(a) "+nomedoaluno+" ficou com média: "+resultado+"!");
+	
+	
+	
+	}
+
+}
